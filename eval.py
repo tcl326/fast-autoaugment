6eacca981a2b291e125c03b85bab03d6fc4b4f69
Ting-Che Lin <ting-che.lin@petuum.com>
2019-10-28 16:03:16 -0400

    can load data parallel model

--- eval.py
+++ eval.py
@@ -2,6 +2,7 @@ import os
 import fire
 import json
 from pprint import pprint
+from collections import OrderedDict
 
 import torch
 import torch.nn as nn
@@ -27,7 +28,10 @@ def eval(model_path):
 
     print('\n[+] Load model')
     weight_path = os.path.join(model_path, 'model', 'model.pt')
-    model.load_state_dict(torch.load(weight_path))
+    try:
+        model.load_state_dict(torch.load(weight_path))
+    except RuntimeError:
+        model.load_state_dict(parse_data_parallel_model(torch.load(weight_path)))
 
     print('\n[+] Load dataset')
     test_transform = get_valid_transform(args, model)
@@ -42,6 +46,14 @@ def eval(model_path):
     print('  Acc@5 : {:.3f}%'.format(_test_res[1].data.cpu().numpy()[0]*100))
     print('  Loss : {:.3f}'.format(_test_res[2].data))
     print('  Infer Time(per image) : {:.3f}ms'.format(_test_res[3]*1000 / len(test_dataset)))
+
+
+def parse_data_parallel_model(state):
+    new_state_dict = OrderedDict()
+    for k, v in state.items():
+        name = k[7:]  # remove `module.`
+        new_state_dict[name] = v
+    return new_state_dict
 
 
 if __name__ == '__main__':
